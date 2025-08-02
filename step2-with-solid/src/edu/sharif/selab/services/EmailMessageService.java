58ac145ecd4ba2d88a6b9734ca345ac67467f892
dorsanaji <Dorsaanaji@gmail.com>
2025-08-02 20:50:20 +0330

    Add EmailService interface and Implement EmailService in EmailMessageService

--- step2-with-solid/src/edu/sharif/selab/services/EmailMessageService.java
+++ step2-with-solid/src/edu/sharif/selab/services/EmailMessageService.java
@@ -1,33 +1,26 @@
 package edu.sharif.selab.services;
 
 import edu.sharif.selab.models.EmailMessage;
-import edu.sharif.selab.models.SmsMessage;
-
 import java.util.regex.Pattern;
 
-public class EmailMessageService implements MessageService{
+public class EmailMessageService implements EmailService {
     @Override
-    public void sendSmsMessage(SmsMessage smsMessage) {
-        //Empty Body
-    }
-
-    @Override
-    public void sendEmailMessage(EmailMessage emailMessage) {
-        if(validateEmailAddress(emailMessage.getSourceEmailAddress()) && validateEmailAddress(emailMessage.getTargetEmailAddress())){
-            System.out.println("Sending a SMS from " + emailMessage.getSourceEmailAddress() + " to " + emailMessage.getTargetEmailAddress() + " with content : " + emailMessage.getContent());
-        }else{
-            throw new IllegalArgumentException("Email Address is Not Correct!");
+    public void sendEmail(EmailMessage email) {
+        if (!validateEmail(email.getSourceEmailAddress())
+                || !validateEmail(email.getTargetEmailAddress())) {
+            System.out.println("⚠️ Invalid email address.");
+            return;
         }
+        System.out.println("📧 Email from "
+                + email.getSourceEmailAddress()
+                + " to "
+                + email.getTargetEmailAddress()
+                + ": "
+                + email.getContent());
     }
 
-    public boolean validateEmailAddress(String email) {
-        // Regular expression pattern for validating email addresses
-        String emailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
-
-        // Compile the pattern into a regex Pattern object
-        Pattern pattern = Pattern.compile(emailRegex);
-
-        // Check if the email string matches the regex pattern
-        return pattern.matcher(email).matches();
+    private boolean validateEmail(String e) {
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        return e != null && Pattern.compile(regex).matcher(e).matches();
     }
 }
---
+++ step2-with-solid/src/edu/sharif/selab/services/EmailService.java
@@ -0,0 +1,8 @@
+// EmailService.java
+package edu.sharif.selab.services;
+
+import edu.sharif.selab.models.EmailMessage;
+
+public interface EmailService {
+    void sendEmail(EmailMessage email);
+}
